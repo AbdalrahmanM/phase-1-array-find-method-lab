d42184fd578ec4f37fabb23d26aa3aeebb89d9d1
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-22 16:51:19 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    let results=record.find(function(element){
+        return element.result==='W'
+    })
+    if(results){
+        return results.year
+    }
+    else
+    {
+        return undefined
+    }
+}
